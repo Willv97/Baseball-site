fe95a724b7a54ecc4cead5185894b58fa3af8432
Willv97 <Willv1357@gmail.com>
2025-05-26 14:53:28 -0400

    hamburger nav with css, function to handle open and close of the nav, videos page added a coming soon sign with a pulse effect

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Hamburger nav to open and close onclick
+  const hamburger = document.getElementById('hamburger');
+  const navLinks = document.getElementById('nav-links');
+
+  hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
+
